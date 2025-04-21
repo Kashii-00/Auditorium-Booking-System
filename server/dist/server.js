3450f206f2e7fc97c58bae4def20e0fdca366bfb
Kashii-00 <165863712+Kashii-00@users.noreply.github.com>
2025-04-21 16:03:51 +0530

    first commit

--- server/dist/server.js
+++ server/dist/server.js
@@ -31,9 +31,17 @@ if (DEBUG) {
 }
 
 // CORS configuration to support credentials
+const allowedOrigins = ['http://10.70.4.34:3000', 'http://10.70.4.34:3060'];
 const corsOptions = {
-  origin: 'http://10.70.4.34:3000',
-  // Your client application URL
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   // Required for cookies
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
